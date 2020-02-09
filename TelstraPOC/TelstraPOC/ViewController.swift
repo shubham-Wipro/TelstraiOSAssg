c58b398034a91a377e608174bd5549fb8ff673b7
SH325448 <shubham.yadav2@wipro.com>
2020-02-09 15:51:53 +0530

    New Proj

---
+++ TelstraPOC/TelstraPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelstraPOC
+//
+//  Created by Shubh on 09/02/20.
+//  Copyright © 2020 Shubh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
